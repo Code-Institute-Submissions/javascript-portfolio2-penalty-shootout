6b21c3c9cbd2beae92a193e881bca459e1667b59
0w3nM <>
2023-05-11 14:06:02 +0000

    Restructured html creating new buttons, added new image and removed one.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+const buttons = document.getElementByClassName("control");
+const playerScore = document.getElementById("player-score");
+const opponentScore = document.getElementById("opponent-score");
+const messages = document.getElementById("messages"):
+const choices = ["top-left", "mid-left","bottom-left","top-mid","mid-mid","bottom-mid","top-right","mid-right","bottom-right"];
 
-    for (let button of buttons) {
+ for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
+            let playerChoice = this.getAttribute("data-choice");
+            playGame(playerChoice);
         });
     }
-});
-runGame(){
+            
+function playGame(playerChoice){
+    let computerChoice = Math.floor(Math.random()* 8) +1;
+
+    let result = CheckWinner(choices[ComputerChoice], choices[palyerChoice]);
+
+    updateScores(result);
+}
+function checkAnswer(){
 
 }
-checkAnswer(){
+function calculateCorrectAnswer(){
 
 }
-calculateCorrectAnswer(){
-
+function incrementGoalScore(){
+    let oldScore = parseInt(document.getElementById("scored").innerText);
+    document.getElementById("scored").innerText = ++oldScore;
 }
-incremntGoalScore(){
-
-}
-incrementSavedScore(){
-
+function incrementSavedScore(){
+    let oldScore = parseInt(document.getElementById("saved").innerText);
+    document.getElementById("saved").innerText = ++oldScore;
 }
