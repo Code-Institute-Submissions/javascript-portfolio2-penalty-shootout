07cc3fef525dee0b663f16b1cae4afc7858e0d8e
0w3nM <>
2023-05-11 19:53:27 +0000

    Added more css stylings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
 const buttons = document.getElementByClassName("control");
 const playerScore = document.getElementById("player-score");
 const opponentScore = document.getElementById("opponent-score");
-const messages = document.getElementById("messages"):
-const choices = ["top-left", "mid-left","bottom-left","top-mid","mid-mid","bottom-mid","top-right","mid-right","bottom-right"];
+const messages = document.getElementById("messages");
+const choices = ["top-left", "mid-left", "bottom-left", "top-mid", "mid-mid", "bottom-mid", "top-right", "mid-right", "bottom-right"];
 
- for (let button of buttons) {
-        button.addEventListener("click", function () {
-            let playerChoice = this.getAttribute("data-choice");
-            playGame(playerChoice);
-        });
-    }
-            
-function playGame(playerChoice){
-    let computerChoice = Math.floor(Math.random()* 8) +1;
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
 
-    let result = CheckWinner(choices[ComputerChoice], choices[palyerChoice]);
+function playGame(playerChoice) {
+    let computerChoice = Math.floor(Math.random() * 8) + 1;
+
+    let result = CheckWinner(choices[computerChoice], choices[playerChoice]);
 
     updateScores(result);
 }
-function checkAnswer(){
+function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
-function incrementGoalScore(){
+function incrementGoalScore() {
     let oldScore = parseInt(document.getElementById("player-score").innerText);
     document.getElementById("player-score").innerText = ++oldScore;
 }
-function incrementSavedScore(){
+function incrementSavedScore() {
     let oldScore = parseInt(document.getElementById("opponent-score").innerText);
     document.getElementById("opponent-score").innerText = ++oldScore;
 }
