5ce0442dfe8645428fce51157a8d1e6aeb6477c2
0w3nM <>
2023-05-11 07:25:21 +0000

    Updated button class names.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "shot") {
-                alert("You clicked shot!");
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
