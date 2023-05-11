1248ff852d1ab704d4d99badd6c44bc2ff2ee7e2
0w3nM <>
2023-05-11 07:02:35 +0000

    Changed button names, changed gameType name and padding on score area.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "football") {
-                alert("You clicked football!");
-          } else {
-              let gameType = this.getAttribute("data-type");
-              alert(`You clicked ${gameType}`);
-          }
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "shot") {
+                alert("You clicked shot!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
         });
     }
 });
